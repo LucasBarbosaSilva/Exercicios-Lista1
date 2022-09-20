910c69e156927c3eca77b6097de7026b2d2d5733
Lucas Silva <blucas.oficial@gmail.com>
2022-09-20 19:51:05 -0300

    First exercise

---
+++ exercicio1/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package exercicio1;
+
+public class CurrencyConverter {
+	public static double dollarToReal(double amount, double dollarPrice) {
+		double result = (amount * dollarPrice)*1.06;
+		return result;
+	}
+}
+
+
+
