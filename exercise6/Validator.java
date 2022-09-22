b240b56d1a396867f62d36af62e1f074f8602f57
LucasBarbosaSilva <blucas.oficial@gmail.com>
2022-09-22 16:22:24 -0300

    change: fixed bugs exercise 6

--- exercise6/Validator.java
+++ exercise6/Validator.java
@@ -1,5 +1,11 @@
 package exercise6;
 
 public class Validator {
-  
+  public static void runValidation(IEntityValidation entity){
+    if(entity.isValid()){
+        System.out.println("Válido");
+    }else{
+        System.out.println("Inválido");
+    }
+  }
 }
